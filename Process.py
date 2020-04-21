f6f81193d45b7305a647b952f3cf232f9859fe7b
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2020-04-21 14:50:41 -0400

    json blob processor

--- Process.py
+++ Process.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 def archive():
 	pass
-	# TODO: create archive data bases, insert all info into
+	# TODO: create archive data bases
 def processing():
 	pass
 	# TODO: process json in incoming table, return
 def execute():
 	pass
-	# TODO: insert all info parsed by json, call archive
+	# TODO: insert all info parsed by json and archive info
 if __name__ == "__main__":
 	processing()
 	execute()
