a50ed4239598237178737991b363a85ed69ee1e0
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2020-04-21 14:49:11 -0400

    count normal

--- Process.py
+++ Process.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 def archive():
 	pass
+	# TODO: create archive data bases, insert all info into
 def processing():
 	pass
+	# TODO: process json in incoming table, return
 def execute():
 	pass
+	# TODO: insert all info parsed by json, call archive
 if __name__ == "__main__":
+	processing()
 	execute()
