09032b952ad14bb2185fb14d1b93f4419088f36f
Joon Park <taejoon.park@mail.utoronto.ca>
2020-04-21 15:39:24 -0400

    Added Processing

--- Process.py
+++ Process.py
@@ -1,13 +1,25 @@
 from datetime import datetime
+import mysql.connector
+import psycopg2
+import sys
+
 def archive():
 	pass
 	# TODO: create archive data bases
-def processing():
-	pass
-	# TODO: process json in incoming table, return
-def execute():
-	pass
-	# TODO: insert all info parsed by json and archive info
+def processing(username):
+	conn = psycopg2.connect(user=username, database='odin')
+	cursor = conn.cursor()
+	cursor.execute("SELECT payload from incoming;")
+	incoming_data = []
+	for row in cursor.fetchall():
+		incoming_data.append(row[0])
+	conn.close()
+	return incoming_data      
+	
+def execute(username):
+	incoming_data = processing(username)
+	print(incoming_data)
+# TODO: insert all info parsed by json and archive info
 if __name__ == "__main__":
-	processing()
-	execute()
+	processing(sys.argv[1])
+	execute(sys.argv[1])
