c2f5c339cd3598b821f6c487844acf245f80f8bd
Joon Park <taejoon.park@mail.utoronto.ca>
2020-04-22 15:02:13 -0400

    working on execute

--- Process.py
+++ Process.py
@@ -2,10 +2,12 @@ from datetime import datetime
 import mysql.connector
 import psycopg2
 import sys
+from create import * 
 
 def archive():
 	pass
 	# TODO: create archive data bases
+
 def processing(username):
 	conn = psycopg2.connect(user=username, database='odin')
 	cursor = conn.cursor()
@@ -18,6 +20,30 @@ def processing(username):
 	
 def execute(username):
 	incoming_data = processing(username)
+	current_tables = showAllTablesODIN(False, username)
+	#print(incoming_data[0]['name'])
+	#print(current_tables)
+	if (incoming_data[0]['name'] in current_tables):
+		pass
+	elif (incoming_data[0]['name'] == 'Grouper'):
+		var_dict = {}
+		max_attribute_len = 0
+		index = 0
+		for column in incoming_data:
+			if (len(column) > max_attribute_len):
+				max_attribute_len = len(column)
+			index++
+		max_data_index = incoming_data.index(max(incoming_data))
+		for column in incoming_data[max_data_index]:
+			if (type(incoming_data[max_data_index][column]) == type({})):
+				var_dict['stemname'] = 'varchar'
+				var_dict['numstems'] = 'Int'
+			else:
+				var_dict[column] = 'varchar'
+		createTable(table_name, var_dict, username)
+		
+
+	
 	
 # TODO: insert all info parsed by json and archive info
 if __name__ == "__main__":
