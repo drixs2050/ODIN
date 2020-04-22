42ac9ea2030eb8f6ad467a398b8fb8b431d613fb
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2020-04-22 14:32:57 -0400

    Merge branch 'master' of C:\Users\drixs\Documents\ODIN with conflicts.

--- Process.py
+++ Process.py
@@ -1,25 +1,13 @@
 from datetime import datetime
-import mysql.connector
-import psycopg2
-import sys
-
 def archive():
 	pass
 	# TODO: create archive data bases
-def processing(username):
-	conn = psycopg2.connect(user=username, database='odin')
-	cursor = conn.cursor()
-	cursor.execute("SELECT payload from incoming;")
-	incoming_data = []
-	for row in cursor.fetchall():
-		incoming_data.append(row[0])
-	conn.close()
-	return incoming_data      
-	
-def execute(username):
-	incoming_data = processing(username)
-	
-# TODO: insert all info parsed by json and archive info
+def processing():
+	pass
+	# TODO: process json in incoming table, return
+def execute():
+	pass
+	# TODO: insert all info parsed by json and archive info
 if __name__ == "__main__":
-	processing(sys.argv[1])
-	execute(sys.argv[1])
+	processing()
+	execute()
