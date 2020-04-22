56281b9f3b3eceb003bcc809a09816d60ca8b778
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2020-04-22 14:59:38 -0400

    json blob processor

--- Process.py
+++ Process.py
@@ -1,13 +1,31 @@
 from datetime import datetime
-def archive():
-	pass
-	# TODO: create archive data bases
-def processing():
-	pass
-	# TODO: process json in incoming table, return
-def execute():
-	pass
-	# TODO: insert all info parsed by json and archive info
+from create import *
+import psycopg2
+def archive(username):
+	conn = psycopg2.connect(user=username, database='odin')
+	cursor = conn.cursor()
+	cursor.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'archive');")
+	if cursor.fetchone()[0] == 'f':
+		attr = {"payload": "json", "processed_by": "varchar", "processed_on": "datetime", "archived on": "datetime"}
+		createTable("archive", attr, username)
+
+
+def processing(username):
+	conn = psycopg2.connect(user=username, database='odin')
+	cursor = conn.cursor()
+	cursor.execute("SELECT payload from incoming;")
+	incoming_data = []
+	for row in cursor.fetchall():
+		incoming_data.append(row[0])
+	conn.close()
+	return incoming_data
+
+
+def execute(username):
+	incoming_data = processing(username)
+
+
 if __name__ == "__main__":
+
 	processing()
 	execute()
