a65214cce3d3b5db02cc5ad0f6e7b2ae45fb4b2d
Junda Zhao <junda.zhao@mail.utoronto.ca>
2020-06-01 16:24:26 -0400

    test

--- vpntest.py
+++ vpntest.py
@@ -11,8 +11,9 @@ for line in ssh.stdout:
     info = {}
     try:
         json['Date'] = stripped[0][0:6].strip()
+        json['Service_version'] = stripped[0][23:36].strip()
         info['Time'] = stripped[0][7:15].strip()
-        info['Service'] = stripped[0][16:36].strip()
+        info['Service'] = stripped[0][16:21].strip()
         info['Group']= stripped[0][46:].strip()
         info['IP'] = stripped[2][5:].strip()
         info['Session Type'] = stripped[4][14:].strip()
